e0462361ff142b513908225bd4d03967acebb02a
Joseph Eiba <josepheiba@icloud.com>
2024-09-02 21:22:15 +0100

    adding api veriable

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,7 +22,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized_error(error):
+def unauthorized_error(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
