b6490a0685dd51b9aec64b511c5608aee62fc9f2
Joseph Eiba <josepheiba@icloud.com>
2024-09-03 19:08:45 +0100

    adding api veriable

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,7 +49,6 @@ class BasicAuth(Auth):
             return (None, None)
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
-
         user = decoded_base64_authorization_header.split(':', 1)[0]
         password = decoded_base64_authorization_header.split(':', 1)[1]
         return (user, password)
