8d7a4fdde27ba4564985fed2039cd7503b8b4cbd
Joseph Eiba <josepheiba@icloud.com>
2024-09-03 14:46:02 +0100

    adding api veriable

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+from flask import request
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method that should implement the logic for checking if a path
+            needs authentication
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that should implement the logic for checking if a request
+            is authorized
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method that should implement the logic for checking if a request
+            is authorized
+        """
+        return None
