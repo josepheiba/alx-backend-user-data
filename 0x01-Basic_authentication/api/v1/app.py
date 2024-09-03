1dbd58cb70e4e5e320f714e42343b98efcce6949
Joseph Eiba <josepheiba@icloud.com>
2024-09-03 23:40:28 +0100

    adding api veriable

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,22 +22,19 @@ if auth_type and auth_type == "basic_auth":
 
 
 @app.before_request
-def authenticate_user():
-    """Authenticates a user before processing a request.
+def before_request() -> None:
+    """ Before request
     """
-    if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-        ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
-            if user is None:
-                abort(403)
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
 
 
 @app.errorhandler(404)
