587f8d0b5ee4b763076c9dbe197edef0e448084a
Joseph Eiba <josepheiba@icloud.com>
2024-09-03 18:29:20 +0100

    adding api veriable

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,8 +17,8 @@ auth = None
 auth_type = getenv("AUTH_TYPE")
 
 if auth_type and auth_type == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
