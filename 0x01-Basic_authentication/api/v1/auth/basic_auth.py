2008a9d07efa21a73ff331ce4a6f559deda06f97
Joseph Eiba <josepheiba@icloud.com>
2024-09-03 18:40:00 +0100

    adding api veriable

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,9 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
