38f15c796bf06f11e9cb1b3fe4d9bc6f48f232d2
Joseph Eiba <josepheiba@icloud.com>
2024-09-06 00:47:01 +0100

    added file extention error check

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -42,12 +43,8 @@ class SessionAuth(Auth):
         del self.user_id_by_session_id[session_id]
         return True
 
-    def current_user(self, request=None):
-        """ Retrieves a User instance based on a cookie value """
-        if request is None:
-            return None
-        session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
-            return None
+    def current_user(self, request=None) -> User:
+        """Retrieves the user associated with the request.
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
